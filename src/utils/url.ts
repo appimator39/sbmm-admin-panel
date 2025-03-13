80fae4f372e60e17cd31c5e628686cbf416387e2
appimators37 <webdev_1@anchorwebdesigns.com>
2025-03-13 21:50:29 +0500

    Added URL

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://127.0.0.1:3030';
+export const BASE_URL = 'http://owsokwwo8w448ok40wksg8ok.85.190.241.20.sslip.io';
 export const LOGIN_URL = `${BASE_URL}/auth/login`;
