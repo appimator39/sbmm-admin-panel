187accf1d60936f06cb99a83230dbdf2663ac214
appimator39 <appimators@gmail.com>
2025-03-18 11:50:11 +0500

    Update url.ts

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,6 @@
-export const BASE_URL = 'https://portal.sbmm.com.pk';
-//export const BASE_URL = 'http://localhost:3040';
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://portal.sbmm.com.pk'
+    : 'http://localhost:3040';
 
 export const LOGIN_URL = `${BASE_URL}/auth/login`;
